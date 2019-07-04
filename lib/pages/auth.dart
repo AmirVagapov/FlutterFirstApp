0b784b6f54e118c037645dcd6df91620dc984600
galt <galt@ecwid.com>
2019-07-05 02:30:41 +0400

    Updated product item view

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -20,36 +20,63 @@ class _AuthPageState extends State<AuthPage> {
           title: Text("Authentification"),
         ),
         body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5), BlendMode.dstATop),
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/background.jpg"))),
             padding: EdgeInsets.only(left: 10.0, right: 10.0),
             child: Center(
-              child: ListView(children: [
+                child: SingleChildScrollView(
+              child: Column(children: [
+                SizedBox(height: 10.0),
                 TextField(
                   keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(labelText: "Enter your email"),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 1.0, color: Colors.white)),
+                      labelText: "Enter your email",
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.7)),
                   onChanged: (String value) {
                     setState(() {
                       _email = value;
                     });
                   },
                 ),
+                SizedBox(height: 10.0),
                 TextField(
                   obscureText: true,
-                  decoration: InputDecoration(labelText: "Enter password"),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 1.0, color: Colors.grey)),
+                      labelText: "Enter password",
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.7)),
                   onChanged: (String value) {
                     setState(() {
                       _password = value;
                     });
                   },
                 ),
-                SwitchListTile(
-                  title: Text("Accept Terms and Pricay Policy"),
-                  value: _acceptTerms,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _acceptTerms = value;
-                    });
-                  },
-                ),
+                SizedBox(height: 10.0),
+                DecoratedBox(
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.7),
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        border: Border.all(color: Colors.grey, width: 1.0)),
+                    child: SwitchListTile(
+                      title: Text("Accept Terms and Pricay Policy"),
+                      value: _acceptTerms,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _acceptTerms = value;
+                        });
+                      },
+                    )),
                 SizedBox(height: 10.0),
                 RaisedButton(
                   color: Theme.of(context).primaryColor,
@@ -63,6 +90,6 @@ class _AuthPageState extends State<AuthPage> {
                   },
                 ),
               ]),
-            )));
+            ))));
   }
 }
